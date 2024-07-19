4150430c96a05424cb7956bdbb5fad2c9d264b0e
Nguyễn Văn Ngọc <ngoc.nv0410@gmail.com>
2024-07-19 12:21:14 +0700

    Add logic employee management

--- src/components/employees/Employee.js
+++ src/components/employees/Employee.js
@@ -1,97 +1,209 @@
 import { useEffect, useState } from "react";
 import { DeleteOutlined, SearchOutlined, UserAddOutlined, UsergroupAddOutlined } from "@ant-design/icons";
-import { Button, Col, Divider, Flex, Input, Modal, Typography, Form, Row, Select, message, Table, Space } from "antd"
+import { Button, Textarea, Divider, Flex, Input, Modal, Typography, Form, Row, Select, message, Table, Space } from "antd"
 
 import styles from './Employee.module.scss'
-import { render } from "@testing-library/react";
+import TextArea from "antd/es/input/TextArea";
 import clsx from "clsx";
 import axios from "axios";
-const { Title } = Typography
+import { Await } from "react-router-dom";
+const { Title, Text } = Typography
 const { Column, ColumnGroup } = Table
-const columns = [
-    {
-        title: 'HỌ VÀ TÊN',
-        dataIndex: 'fullName',
-        key: 'fullName',
-    },
-    {
-        title: 'EMAIL',
-        dataIndex: 'email',
-        key: 'email',
-    },
-    {
-        title: 'SỐ ĐIỆN THOẠI',
-        dataIndex: 'phoneNumber',
-        key: 'phoneNumber',
-    },
-    {
-        title: 'NHÓM',
-        dataIndex: 'teamName',
-        key: 'teamName',
-    },
-    {
-        title: 'THAO TÁC',
-        key: 'action',
-        render: () => (
-            <Space size="middle">
-              <Button className={clsx(styles.btn, styles.changeManagementBtn)} shape="circle">{<UserAddOutlined />}</Button>
-              <Button className={clsx(styles.btn, styles.addRoleBtn)} shape="circle">{<UsergroupAddOutlined />}</Button>
-              <Button className={clsx(styles.btn, styles.deleteBtn)} shape="circle">{<DeleteOutlined />}</Button>
-            </Space>
-          ),
-    },
-]
-
-const data = [
-    {
-      key: '1',
-      fullName: 'John Brown',
-      email: 'email01@gmail.com',
-      phoneNumber: '0972549878',
-      teamName: 'Teachnical',
-    },
-    {
-        key: '2',
-        fullName: 'John Brown',
-        email: 'email01@gmail.com',
-        phoneNumber: '0972549878',
-        teamName: 'Teachnical',
-      },
-      {
-        key: '3',
-        fullName: 'John Brown',
-        email: 'email01@gmail.com',
-        phoneNumber: '0972549878',
-        teamName: 'Teachnical',
-      },
-  ];
 
 const Employee = () => {
-    const[employees, setEmployees] = useState([])
-    const[isChange, setIsChange] = useState(false)
+    const token = localStorage.getItem('token')
+    const instance = axios.create({
+        baseURL: 'https://localhost:44389',
+        timeout: 5000,
+        headers: { 'Authorization': `Bearer ${token}` }
+    });
 
+    const [departments, setDepartments] = useState([])
+    const [employees, setEmployees] = useState([])
+    const [selectedEmployee, setSelectedEmployee] = useState('')
+    const [isChange, setIsChange] = useState(false)
+
+    const [isEditDepartment, setIsEditDepartment] = useState(false)
+    const [isEditRoles, setIsEditRoles] = useState(false)
+    const [isOpenDelete, setIsOpenDelete] = useState(false)
+
+    const [formData, setFormData] = useState({})
+
+    // Logic get data when change data input
+    const onChangeSelect = (value) => {
+        setFormData(prev => {
+            return {
+                ...prev,
+                teamId: value,
+            }
+        })
+    }
+
+    // Logic get All Users
     useEffect(() => {
-        const fetchData = async() => {
+        const fetchData = async () => {
             const response = await axios.get('https://localhost:44389/api/User/GetAllUsers')
-            if(response && response.status === 200){
-                    let data1 = []
-                    response.data.forEach((element, index) => {
-                        const {fullName, email, phoneNumber, teamName } = element;
-                        data1.push({
-                            key: index,
-                            fullName,
-                            email,
-                            phoneNumber,
-                            teamName,
-                        })
-                    });
-                setEmployees(data1)
+            if (response && response.status === 200) {
+                let data = []
+                response.data.forEach((element, index) => {
+                    const { fullName, email, phoneNumber, teamName, id } = element;
+                    data.push({
+                        key: id,
+                        fullName,
+                        email,
+                        phoneNumber,
+                        teamName,
+                    })
+                });
+                setEmployees(data)
+                setIsChange(false)
             }
         }
 
         fetchData();
     }, [isChange])
-    console.log(employees)
+    // console.log(employees)
+
+    // Logic get All Departments
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://localhost:44389/api/Admin/GetAllTeams')
+
+                if (response && response.status === 200) {
+                    setDepartments(prev => {
+                        const resultData = response.data.map(item => {
+                            const { name, id } = item
+                            return {
+                                value: id,
+                                label: name,
+                            }
+                        })
+                        return resultData
+                    })
+                }
+            } catch (error) {
+                console.error('Có lỗi xảy ra: ', error)
+            }
+        }
+
+        fetchData();
+    }, [isChange])
+    // console.log(departments);
+
+    // Logic Edit employee's department
+    const openEditDepartment = (data) => {
+        setIsEditDepartment(true)
+        setSelectedEmployee(data.fullName)
+        setFormData(prev => {
+            return {
+                ...prev,
+                employeeId: data.key,
+            }
+        })
+    }
+    const closeEditDepartment = () => {
+        setIsEditDepartment(false);
+        setSelectedEmployee('');
+        setFormData({});
+    }
+    // console.log(formData)
+    const handleEditDepartment = async () => {
+        try {
+            const response = await instance.put('api/Admin/ChangeDepartmentForUser', formData)
+            console.log(response)
+            setIsChange(true)
+            setIsEditDepartment(false)
+            setFormData({})
+            setSelectedEmployee('')
+        } catch (error) {
+            console.error('Có lỗi không thể cập nhật phòng ban: ', error)
+        }
+    }
+
+    // Logic Edit employee's roles
+    const openEditRoles = (data) => {
+        setIsEditRoles(true)
+        setSelectedEmployee(data.fullName)
+    }
+    const closeEditRoles = () => {
+        setIsEditRoles(false)
+        setSelectedEmployee('')
+        setFormData({});
+    }
+    const handleEditRoles = () => {
+    }
+
+    // Logic Delete employee
+    const openDelete = (data) => {
+        setIsOpenDelete(true)
+    }
+    const closeDelete = () => setIsOpenDelete(false)
+    const handleDelete = async () => {
+        try {
+            const response = await instance.delete(`/api/Admin/DeleteTeam/${formData.teamId}`)
+            if (response && response.status === 200) {
+                setIsChange(true);
+                setIsOpenDelete(false);
+                setFormData({})
+                message.success('Xóa phòng ban thành công')
+            } else (
+                message.error('Không thể xóa phòng ban!')
+            )
+        } catch (error) {
+            console.error('Không thể xóa phòng ban! Lỗi: ', error)
+        }
+    }
+
+    // Define Collumn of Table
+    const columns = [
+        {
+            title: 'HỌ VÀ TÊN',
+            dataIndex: 'fullName',
+            key: 'fullName',
+        },
+        {
+            title: 'EMAIL',
+            dataIndex: 'email',
+            key: 'email',
+        },
+        {
+            title: 'SỐ ĐIỆN THOẠI',
+            dataIndex: 'phoneNumber',
+            key: 'phoneNumber',
+        },
+        {
+            title: 'NHÓM',
+            dataIndex: 'teamName',
+            key: 'teamName',
+        },
+        {
+            title: 'THAO TÁC',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <Button className={clsx(styles.btn, styles.changeManagementBtn)}
+                        shape="circle"
+                        onClick={() => openEditDepartment(record)}
+                    >
+                        {<UserAddOutlined />}
+                    </Button>
+                    <Button className={clsx(styles.btn, styles.addRoleBtn)}
+                        shape="circle"
+                        onClick={() => openEditRoles(record)}
+                    >
+                        {<UsergroupAddOutlined />}
+                    </Button>
+                    <Button className={clsx(styles.btn, styles.deleteBtn)}
+                        shape="circle"
+                        onClick={openDelete}
+                    >
+                        {<DeleteOutlined />}
+                    </Button>
+                </Space>
+            ),
+        },
+    ]
 
     return (
         <>
@@ -100,6 +212,67 @@ const Employee = () => {
                     <Input prefix={<SearchOutlined />} className='searchInput' placeholder="Tìm kiếm nhân viên" />
                     <Button className="submitBtn">Tìm kiếm</Button>
                 </Flex>
+
+                <Modal
+                    className="modal"
+                    open={isEditDepartment}
+                    onCancel={closeEditDepartment}
+                    footer={null}
+                    title='Cập nhật phòng ban nhân viên'
+                >
+                    <Text>Nhân viên: {selectedEmployee}</Text>
+                    <Form layout="vertical" onFinish={handleEditDepartment}>
+                        <Form.Item
+                            label='Phòng ban'
+                        >
+                            <Select
+                                name='teamId'
+                                options={departments}
+                                value={formData.teamId}
+                                onChange={onChangeSelect}
+                            >
+                            </Select>
+                        </Form.Item>
+                        <Form.Item>
+                            <Flex gap='middle'>
+                                <Button htmlType="submit" type="primary">Cập nhật</Button>
+                                <Button onClick={closeEditDepartment}>Thoát</Button>
+                            </Flex>
+                        </Form.Item>
+                    </Form>
+                </Modal>
+
+                <Modal
+                    className="modal"
+                    open={isEditRoles}
+                    onCancel={closeEditRoles}
+                    title='Cập nhật quyền hạn'
+                >
+                    <Text>Nhân viên: {selectedEmployee}</Text>
+                    <Form layout="vertical">
+                        <Form.Item
+                            label='Quyền hạn'
+                        >
+                            <Select
+                                name='permission'
+                            >
+                            </Select>
+                        </Form.Item>
+                    </Form>
+                </Modal>
+
+                <Modal
+                    className="modal"
+                    open={isOpenDelete}
+                    onCancel={closeDelete}
+                    footer={null}
+                    title='Bạn chắc chắn xóa nhân viên này này?'
+                >
+                    <Flex gap='middle'>
+                        <Button onClick={handleDelete} type="primary">Xóa</Button>
+                        <Button onClick={closeDelete}>Thoát</Button>
+                    </Flex>
+                </Modal>
                 <Table className={styles.tableEmployee} columns={columns} dataSource={employees} />
             </Flex>
         </>
